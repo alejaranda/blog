6faa88a7ad1cecd4541078e6c49557a9944d34a3
Alejandro <alejandro.arancibia.aranda@gmail.com>
2026-07-27 14:37:20 -0400

    chore: set up internationalization

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,33 @@
+import { notFound } from "next/navigation";
+import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { setRequestLocale } from "next-intl/server";
+import { routing } from "@/i18n/routing";
+import "../globals.css";
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  setRequestLocale(locale);
+
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <body>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ i18n/request.ts
@@ -0,0 +1,25 @@
+import { getRequestConfig } from "next-intl/server";
+import { hasLocale } from "next-intl";
+
+import { routing } from "./routing";
+
+import en from "@/messages/en.json";
+import es from "@/messages/es.json";
+
+const messages = {
+  en,
+  es,
+};
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  const requested = await requestLocale;
+
+  const locale = hasLocale(routing.locales, requested)
+    ? requested
+    : routing.defaultLocale;
+
+  return {
+    locale,
+    messages: messages[locale],
+  };
+});
